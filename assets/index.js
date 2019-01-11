e003b4c49d2083b7edbfeb4d598df3398080b709
David Starr <dstarrtrey@gmail.com>
2019-01-10 16:31:19 -0800

    edits js

--- assets/index.js
+++ assets/index.js
@@ -14,7 +14,19 @@ function requestContent(file) {
 }
 
 $("window").on("popstate", function(link){
+  var character = link.state;
 
+  if (character == null) {
+    removeCurrentClass();
+    textWrapper.innerHTML = " ";
+    content.innerHTML = " ";
+    document.title = defaultTitle;
+  } else {
+      updateText(character);
+      requestContent(character + ".html");
+      addCurrentClass(character);
+      document.title = "Ghostbuster | " + character;
+  }
 });
 
 
